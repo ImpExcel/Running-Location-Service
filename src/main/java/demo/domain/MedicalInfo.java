567c700f8d33e8697f20e3cd5bb81cd01f22a16e
ImpExcel <czjpercy@gmail.com>
2017-05-29 23:47:57 -0700

    Running location service code upload

---
+++ src/main/java/demo/domain/MedicalInfo.java
@@ -0,0 +1,16 @@
+package demo.domain;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+import javax.persistence.Embeddable;
+
+@Data
+@RequiredArgsConstructor
+@Embeddable
+public class MedicalInfo {
+
+    private final long bfr;
+
+    private final long fmi;
+}
